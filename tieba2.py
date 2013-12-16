4231de30071ac99c20e494dba60d281c45e77bc3
nmalt <nmalt@qq.com>
2013-12-16 18:47:44 +0800

    Update tieba2.py
    使用正则表达式筛选网页内容进而优化程序

--- tieba2.py
+++ tieba2.py
@@ -9,6 +9,34 @@ import string
 import urllib2
 import re
 
+
+#----------- 处理页面上的各种标签 -----------  
+class HTML_Tool:  
+    # 用非 贪婪模式 匹配 \t 或者 \n 或者 空格 或者 超链接 或者 图片  
+    BgnCharToNoneRex = re.compile("(\t|\n| |<a.*?>|<img.*?>)")  
+      
+    # 用非 贪婪模式 匹配 任意<>标签  
+    EndCharToNoneRex = re.compile("<.*?>")  
+  
+    # 用非 贪婪模式 匹配 任意<p>标签  
+    BgnPartRex = re.compile("<p.*?>")  
+    CharToNewLineRex = re.compile("(<br/>|</p>|<tr>|<div>|</div>)")  
+    CharToNextTabRex = re.compile("<td>")  
+  
+    # 将一些html的符号实体转变为原始符号  
+    replaceTab = [("<","<"),(">",">"),("&","&"),("&","\""),(" "," ")]  
+      
+    def Replace_Char(self,x):  
+        x = self.BgnCharToNoneRex.sub("",x)  
+        x = self.BgnPartRex.sub("\n    ",x)  
+        x = self.CharToNewLineRex.sub("\n",x)  
+        x = self.CharToNextTabRex.sub("\t",x)  
+        x = self.EndCharToNoneRex.sub("",x)  
+ 
+        for t in self.replaceTab:    
+            x = x.replace(t[0],t[1])    
+        return x    
+
     
 class Baidu_Spider:
     # 申明相关的属性
